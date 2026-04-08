7bd2ae6e47adb46f5bc6ae9a5bff96336c608e83
Pamelasibaja25 <62737570+Pamelasibaja25@users.noreply.github.com>
2026-04-07 19:34:26 -0600

    Registro de Clientes Agregado

---
+++ G2_SC603_KN_Proyecto/G2_SC603_KN_Proyecto/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using G2_SC603_KN_Proyecto.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace G2_SC603_KN_Proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Mensaje"] = "Bienvenido";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ G2_SC603_KN_Proyecto/G2_SC603_KN_Proyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G2_SC603_KN_Proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
