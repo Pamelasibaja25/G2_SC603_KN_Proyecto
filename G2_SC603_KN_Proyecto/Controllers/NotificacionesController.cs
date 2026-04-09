9bee5dc53d44b5361d5c71bbae7353604fee3490
OliverRoman20 <144956650+OliverRoman20@users.noreply.github.com>
2026-04-09 09:40:06 -0600

    Agrega controladores y vistas de Notificaciones y Pagos

---
+++ G2_SC603_KN_Proyecto/Controllers/NotificacionesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G2_SC603_KN_Proyecto.Controllers
+{
+    public class NotificacionesController : Controller
+    {
+        // GET: NotificacionesController
+        public ActionResult Index()
+        {
+            ViewData["Mensaje"] = "Notificaciones";
+            return View();
+        }
+
+    }
+}
---
+++ G2_SC603_KN_Proyecto/Controllers/PagosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G2_SC603_KN_Proyecto.Controllers
+{
+    public class PagosController : Controller
+    {
+        // GET: PagosController
+        public ActionResult Index()
+        {
+            ViewData["Mensaje"] = "Control de Pagos";
+            return View();
+
+        }
+        public IActionResult Registrar()
+        {
+            ViewData["Mensaje"] = "Registrar Pago";
+            return View();
+        }
+     
+        }
+
+    }
+
+
