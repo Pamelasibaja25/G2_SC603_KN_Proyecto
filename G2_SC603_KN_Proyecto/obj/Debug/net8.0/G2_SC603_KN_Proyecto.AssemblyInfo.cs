8073949c1e505df0b39db398f4a9e9d9cabade83
OliverRoman20 <144956650+OliverRoman20@users.noreply.github.com>
2026-06-15 10:30:33 -0600

    conflictos

--- G2_SC603_KN_Proyecto/obj/Debug/net8.0/G2_SC603_KN_Proyecto.AssemblyInfo.cs
+++ G2_SC603_KN_Proyecto/obj/Debug/net8.0/G2_SC603_KN_Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G2_SC603_KN_Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994d00df4c868925c8d09c2c57bf220a9809ebdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("G2_SC603_KN_Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G2_SC603_KN_Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
